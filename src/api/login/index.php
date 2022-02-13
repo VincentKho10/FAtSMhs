b0b607566551c6e4a0c650950b4db18ee7824ca1
VincentKho10 <vincentkho10@gmail.com>
2022-02-13 21:03:33 +0700

    login register fix

--- src/api/login/index.php
+++ src/api/login/index.php
@@ -1,11 +1,15 @@
 <?php
 require 'vendor/autoload.php';
-$macaddr = filter_input(1,"macaddr");
 
-$client = new MongoDB\Client(
-    'mongodb+srv://Vincent:Tu70r14l@cluster0.zfifs.mongodb.net/fatsdb?retryWrites=true&w=majority');
+$macaddr = filter_input(1, "macaddr");
 
-$collection_mahasiswa = $client->fatsdb->mahasiswa;
-$result_mahasiswa = $collection_mahasiswa->find([ 'mac_address' => $macaddr ] );
+if ($macaddr != null) {
+    $client = new MongoDB\Client(
+        'mongodb+srv://Vincent:Tu70r14l@cluster0.zfifs.mongodb.net/fatsdb?retryWrites=true&w=majority'
+    );
 
-echo $result_mahasiswa;
+    $collection_mahasiswa = $client->fatsdb->mahasiswa;
+    $result_mahasiswa = $collection_mahasiswa->find(['mac_address' => $macaddr]);
+
+    echo $result_mahasiswa;
+}
--- src/api/registrasi/index.php
+++ src/api/registrasi/index.php
@@ -1,15 +1,19 @@
 <?php
 require 'vendor/autoload.php';
-$nrp = filter_input(1,"nrp");
-$pass = filter_input(1,"pass");
-$macaddr = filter_input(1,"mac");
 
-$client = new MongoDB\Client(
-    'mongodb+srv://Vincent:Tu70r14l@cluster0.zfifs.mongodb.net/fatsdb?retryWrites=true&w=majority');
+$nrp = filter_input(1, "nrp");
+$pass = filter_input(1, "pass");
+$macaddr = filter_input(1, "mac");
 
-$collection_mahasiswa = $client->fatsdb->mahasiswa;
-$result_mahasiswa = $collection_mahasiswa->find([ 'nrp' => $nrp, 'password' => $pass ] );
-$collection_mahasiswa_user = $client->fatsdb->mahasiswa_user;
-$result_mahasiswa_user = $collection_mahasiswa->insertOne([ 'mahasiswa' => $result_mahasiswa, "mac_address" => $macaddr] );
+if ($nrp != null && $pass != null && $macaddr != null) {
+    $client = new MongoDB\Client(
+        'mongodb+srv://Vincent:Tu70r14l@cluster0.zfifs.mongodb.net/fatsdb?retryWrites=true&w=majority'
+    );
 
-echo $result_mahasiswa_user;
+    $collection_mahasiswa = $client->fatsdb->mahasiswa;
+    $result_mahasiswa = $collection_mahasiswa->find(['nrp' => $nrp, 'password' => $pass]);
+    $collection_mahasiswa_user = $client->fatsdb->mahasiswa_user;
+    $result_mahasiswa_user = $collection_mahasiswa->insertOne(['mahasiswa' => $result_mahasiswa, "mac_address" => $macaddr]);
+
+    echo $result_mahasiswa_user;
+}
