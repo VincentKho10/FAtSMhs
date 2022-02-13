b63e28f5241670e901b5d6b3446f18550423ee7a
VincentKho10 <vincentkho10@gmail.com>
2022-02-13 20:37:07 +0700

    login registrasi

---
+++ src/api/login/index.php
@@ -0,0 +1,11 @@
+<?php
+require 'vendor/autoload.php';
+$macaddr = filter_input(1,"macaddr");
+
+$client = new MongoDB\Client(
+    'mongodb+srv://Vincent:Tu70r14l@cluster0.zfifs.mongodb.net/fatsdb?retryWrites=true&w=majority');
+
+$collection_mahasiswa = $client->fatsdb->mahasiswa;
+$result_mahasiswa = $collection_mahasiswa->find([ 'mac_address' => $macaddr ] );
+
+echo $result_mahasiswa;
---
+++ src/api/registrasi/index.php
@@ -0,0 +1,15 @@
+<?php
+require 'vendor/autoload.php';
+$nrp = filter_input(1,"nrp");
+$pass = filter_input(1,"pass");
+$macaddr = filter_input(1,"mac");
+
+$client = new MongoDB\Client(
+    'mongodb+srv://Vincent:Tu70r14l@cluster0.zfifs.mongodb.net/fatsdb?retryWrites=true&w=majority');
+
+$collection_mahasiswa = $client->fatsdb->mahasiswa;
+$result_mahasiswa = $collection_mahasiswa->find([ 'nrp' => $nrp, 'password' => $pass ] );
+$collection_mahasiswa_user = $client->fatsdb->mahasiswa_user;
+$result_mahasiswa_user = $collection_mahasiswa->insertOne([ 'mahasiswa' => $result_mahasiswa, "mac_address" => $macaddr] );
+
+echo $result_mahasiswa_user;
