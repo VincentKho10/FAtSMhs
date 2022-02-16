b8e8bb7d27b1bcf505d3c3762937a427b8eb8e3e
VincentKho10 <vincentkho10@gmail.com>
2022-02-16 20:07:55 +0700

    test login

--- api/login/index.php
+++ api/login/index.php
@@ -1,15 +1,16 @@
 <?php
-require 'vendor/autoload.php';
+require '../../vendor/autoload.php';
 
 $macaddr = filter_input(1, "macaddr");
 
-if ($macaddr != null) {
-    $client = new MongoDB\Client(
-        'mongodb+srv://Vincent:Tu70r14l@cluster0.zfifs.mongodb.net/fatsdb?retryWrites=true&w=majority'
-    );
+var_dump("test");
+// if ($macaddr != null) {
+//     $client = new MongoDB\Client(
+//         'mongodb+srv://Vincent:Tu70r14l@cluster0.zfifs.mongodb.net/fatsdb?retryWrites=true&w=majority'
+//     );
 
-    $collection_mahasiswa = $client->fatsdb->mahasiswa;
-    $result_mahasiswa = $collection_mahasiswa->find(['mac_address' => $macaddr]);
+//     $collection_mahasiswa = $client->fatsdb->mahasiswa;
+//     $result_mahasiswa = $collection_mahasiswa->findOne(['mac_address' => $macaddr]);
 
-    echo $result_mahasiswa;
-}
+//     var_dump($macaddr);
+// }
