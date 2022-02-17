ddfe5b3c4f820ff4ce4805b67f5ee7e21fac1a71
VincentKho10 <vincentkho10@gmail.com>
2022-02-17 07:20:52 +0700

    adding autoload

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-    $act = filter_input(1, "act");
+    require 'vendor/autoload.php';
+    $act = filter_input(1,"act");
     if($act == "login"){
-        require_once("api\login\index.php");
+        require_once('api\login\index.php');
     }
-    else if($act == "register"){
-        require_once("api\register\index.php");
+    else if($act == 'register'){
+        require_once('api\register\index.php');
     }
 ?>
