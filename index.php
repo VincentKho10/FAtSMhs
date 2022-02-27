ae1f418ac7a020cc02813db796638639f2fa56c3
VincentKho10 <vincentkho10@gmail.com>
2022-02-27 20:31:21 +0700

    html completion

--- index.php
+++ index.php
@@ -2,15 +2,29 @@
 require 'vendor/autoload.php';
 require 'api/login/index.php';
 require 'api/registrasi/index.php';
+
+$act = filter_input(1, "act");
 ?>
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>
+            <?php
+                if ($act == "login") {
+                    echo "login";
+                } else if ($act == 'register') {
+                    echo "register";
+                }
+            ?>
+    </title>
+    </head>
+    <body>
     <?php
-        $act = filter_input(1, "act");
         if ($act == "login") {
             include_once('api\login\index.php');
         } else if ($act == 'register') {
             include_once('api\registrasi\index.php');
         }
     ?>
+    </body>
 </html>
