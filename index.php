ac25f2a90221bdf196060cbb0c9017918f4563ed
VincentKho10 <vincentkho10@gmail.com>
2022-02-28 19:23:52 +0700

    test

--- index.php
+++ index.php
@@ -7,25 +7,41 @@ $act = filter_input(1, "act");
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>
-            <?php
-                if ($act == "login") {
-                    echo "login";
-                } else if ($act == 'register') {
-                    echo "register";
-                }
-            ?>
-        </title>
-    </head>
-    <body>
+
+<head>
+    <title>
         <?php
-            if ($act == "login") {
-                include_once('api\login\index.php');
-            } else if ($act == 'register') {
-                include_once('api\registrasi\index.php');
-            }
+        if ($act == "login") {
+            echo "login";
+        } else if ($act == 'register') {
+            echo "register";
+        }
         ?>
-        
-    </body>
+    </title>
+</head>
+
+<body>
+    <?php
+    if ($act == "login") {
+
+        $macaddr = filter_input(1, "macaddr");
+        if ($macaddr != null) {
+            var_dump($macaddr);
+            $client = new MongoDB\Client(
+                'mongodb+srv://Vincent:Tu70r14l@cluster0.zfifs.mongodb.net/fatsdb?retryWrites=true&w=majority'
+            );
+            var_dump($macaddr + "test");
+
+            $collection_mahasiswa = $client->fatsdb->mahasiswa_user;
+            $result_mahasiswa = $collection_mahasiswa->find(['mac_address' => $macaddr]);
+
+            echo $result_mahasiswa;
+        }
+    } else if ($act == 'register') {
+        include_once('api\registrasi\index.php');
+    }
+    ?>
+
+</body>
+
 </html>
