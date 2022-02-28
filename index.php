57b12b31906e5749060839e51807e48acc171fc6
VincentKho10 <vincentkho10@gmail.com>
2022-03-01 02:25:23 +0700

    test

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 require 'vendor/autoload.php';
-require 'api/login/index.php';
-require 'api/registrasi/index.php';
 
+try {
 $act = filter_input(1, "act");
 $client = new MongoDB\Client('mongodb://Vincent:Tu70r14l@cluster0-shard-00-00.zfifs.mongodb.net:27017,cluster0-shard-00-01.zfifs.mongodb.net:27017,cluster0-shard-00-02.zfifs.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-quesel-shard-0&authSource=admin&retryWrites=true&w=majority');
 
@@ -15,7 +14,7 @@ $client = new MongoDB\Client('mongodb://Vincent:Tu70r14l@cluster0-shard-00-00.zf
         <?php
         if ($act == "login") {
             echo "login";
-        } else if ($act == 'register') {
+        } else if ($act == "register") {
             echo "register";
         }
         ?>
@@ -26,9 +25,12 @@ $client = new MongoDB\Client('mongodb://Vincent:Tu70r14l@cluster0-shard-00-00.zf
     <?php
     if ($act == "login") {
         include_once('api\login\index.php');
-    } else if ($act == 'register') {
+    } else if ($act == "register") {
         include_once('api\registrasi\index.php');
     }
+} catch (\Exception $e) {
+    echo $e;
+}
     ?>
 
 </body>
