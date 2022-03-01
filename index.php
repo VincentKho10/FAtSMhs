17b24a431f93c3d8ae071f38e3cd780c45c27652
VincentKho10 <vincentkho10@gmail.com>
2022-03-01 10:27:31 +0700

    test

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 require 'vendor/autoload.php';
 
 $act = filter_input(1, "act");
+$client = new MongoDB\Client('mongodb://Vincent:Tu70r14l@cluster0-shard-00-00.zfifs.mongodb.net:27017,cluster0-shard-00-01.zfifs.mongodb.net:27017,cluster0-shard-00-02.zfifs.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-quesel-shard-0&authSource=admin&retryWrites=true&w=majority');
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,9 +23,43 @@ $act = filter_input(1, "act");
 <body>
     <?php
     if ($act == "login") {
-        include_once('api\login\index.php');
+        try{
+            $macaddr = filter_input(1, "macaddr");
+            if ($macaddr != null) {
+                var_dump($macaddr);
+                $collection_mahasiswa = $client->fatsdb->mahasiswa_user;
+                $result_mahasiswa = $collection_mahasiswa->find(['mac_address'=> $macaddr])->toArray();
+                if(count($result_mahasiswa)==1){
+                    echo MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($result_mahasiswa[0]));
+                }else{
+                    echo "{}";
+                }
+            }
+        } catch (\Exception $e) {
+            throw $e;
+            var_dump($e);
+            echo $e;
+        }
     } else if ($act == "register") {
-        include_once('api\registrasi\index.php');
+        try{
+            $nrp = filter_input(1,"nrp");
+            $pass = filter_input(1,"pass");
+            $macaddr = filter_input(1,"mac");
+        
+            if ($nrp != null && $pass != null && $macaddr != null) {
+                $collection_mahasiswa = $client->fatsdb->mahasiswa;
+                $result_mahasiswa = $collection_mahasiswa->find(['nrp' => $nrp, 'password' => $pass]);
+                $collection_mahasiswa_user = $client->fatsdb->mahasiswa_user;
+                $result_mahasiswa_user = $collection_mahasiswa->insertOne(['mahasiswa' => $result_mahasiswa, "mac_address" => $macaddr]);
+        
+                echo $result_mahasiswa_user;
+            }
+        
+        } catch (\Exception $e) {
+            throw $e;
+            var_dump($e);
+            echo $e;
+        }
     }
     ?>
 
