0c16722d986e3b2928d9ca5d319974e92cc55cfd
VincentKho10 <vincentkho10@gmail.com>
2022-03-01 08:34:30 +0700

    test

--- api/login/index.php
+++ api/login/index.php
@@ -1,14 +1,12 @@
 <?php
 try{
 $macaddr = filter_input(1, "macaddr");
-if ($macaddr != null) {
-    var_dump($macaddr);
-    $collection_mahasiswa = $client->fatsdb->mahasiswa_user;
-    $result_mahasiswa = $collection_mahasiswa->find(['mac_address'=> $macaddr]);
-    var_dump($result_mahasiswa);
-    echo $result_mahasiswa;
-}
-
+    if ($macaddr != null) {
+        var_dump($macaddr);
+        $collection_mahasiswa = $client->fatsdb->mahasiswa_user;
+        $result_mahasiswa = $collection_mahasiswa->find(['mac_address'=> $macaddr])->toArray();
+        var_dump($result_mahasiswa[0]);
+    }
 } catch (\Exception $e) {
     throw $e;
     var_dump($e);
