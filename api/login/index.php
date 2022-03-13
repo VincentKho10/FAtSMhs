a83b4a12bd27c0fff80c59c54870c1f07ff9356f
VincentKho10 <vincentkho10@gmail.com>
2022-03-13 11:11:55 +0700

    login external script

---
+++ api/login/index.php
@@ -0,0 +1,15 @@
+<?php
+try{
+    $macaddr = filter_input(1, "macaddr");
+    if ($macaddr != null) {
+        $collection_mahasiswa = $client->fatsdb->mahasiswa_user;
+        $result_mahasiswa = $collection_mahasiswa->find(['mac_address'=> $macaddr])->toArray();
+        if(count($result_mahasiswa)==1){
+            echo MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($result_mahasiswa[0]));
+        }else{
+            echo "{}";
+        }
+    }
+} catch (\Exception $e) {
+    echo "{}";
+}
--- index.php
+++ index.php
@@ -23,20 +23,7 @@ $client = new MongoDB\Client('mongodb://Vincent:Tu70r14l@cluster0-shard-00-00.zf
 <body>
     <?php
     if ($act == "login") {
-        try{
-            $macaddr = filter_input(1, "macaddr");
-            if ($macaddr != null) {
-                $collection_mahasiswa = $client->fatsdb->mahasiswa_user;
-                $result_mahasiswa = $collection_mahasiswa->find(['mac_address'=> $macaddr])->toArray();
-                if(count($result_mahasiswa)==1){
-                    echo MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($result_mahasiswa[0]));
-                }else{
-                    echo "{}";
-                }
-            }
-        } catch (\Exception $e) {
-            echo "{}";
-        }
+        include_once("api/login/index.php");
     } else if ($act == "register") {
         try{
             $nrp = filter_input(1,"nrp");
