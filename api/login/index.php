890237efa7d44e9814cb6dea92f209bf45cd9480
VincentKho10 <vincentkho10@gmail.com>
2022-03-14 23:41:16 +0700

    test

--- api/login/index.php
+++ api/login/index.php
@@ -2,7 +2,7 @@
 try{
     $macaddr = filter_input(1, "macaddr");
     if ($macaddr != null) {
-        $collection_mahasiswa = $client->fatsdb->mahasiswa_user;
+        $collection_mahasiswa = $client->fatsdb->mahasiswa;
         $result_mahasiswa = $collection_mahasiswa->find(['mac_address'=> $macaddr])->toArray();
         if(count($result_mahasiswa)==1){
             echo MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($result_mahasiswa[0]));
