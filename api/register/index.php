0e3a397289a1d87ec12c569ae7f68a95f63f67f9
VincentKho10 <vincentkho10@gmail.com>
2022-04-04 03:36:36 +0700

    fix all

--- api/register/index.php
+++ api/register/index.php
@@ -8,10 +8,10 @@ try{
         $collection_mahasiswa = $client->fatsdb->mahasiswa;
         $result_mahasiswa = $collection_mahasiswa->find(['nrp' => $nrp, 'password' => $pass])[0];
 		if($result_mahasiswa != null){
-        $collection_mahasiswa_user = $client->fatsdb->mahasiswa_user;
-        // $result_mahasiswa_user = $collection_mahasiswa->insertOne(['mahasiswa' => $result_mahasiswa, "mac_address" => $macaddr]);
-        echo MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($result_mahasiswa));
-		}
+            $collection_mahasiswa_user = $client->fatsdb->mahasiswa_user;
+            // $result_mahasiswa_user = $collection_mahasiswa->insertOne(['mahasiswa' => $result_mahasiswa, "mac_address" => $macaddr]);
+            echo MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($result_mahasiswa));
+        }
     }
 
 } catch (\Exception $e) {
