7f31d5483c58171f53e04883a639ae763877de68
VincentKho10 <vincentkho10@gmail.com>
2022-04-04 04:03:22 +0700

    good

--- api/register/index.php
+++ api/register/index.php
@@ -8,11 +8,12 @@ try{
         $collection_mahasiswa = $client->fatsdb->mahasiswa;
         $result_mahasiswa = $collection_mahasiswa->find(['nrp' => $nrp, 'password' => $pass])[0];
 		if($result_mahasiswa != null){
-            header("Content-type:application/json");
             $collection_mahasiswa_user = $client->fatsdb->mahasiswa_user;
             // $result_mahasiswa_user = $collection_mahasiswa->insertOne(['mahasiswa' => $result_mahasiswa, "mac_address" => $macaddr]);
             echo MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($result_mahasiswa));
         }
+    }else{
+        echo "{}";
     }
 
 } catch (\Exception $e) {
--- index.php
+++ index.php
@@ -21,19 +21,19 @@ $client = new MongoDB\Client('mongodb://Vincent:google.com@cluster0-shard-00-00.
             ?>
         </title>
         <!-- Google Font: Source Sans Pro -->
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+        <!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css"> -->
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
     </head>
 
-    <body class="sidebar-mini layout-boxed sidebar-collapse" style="height: auto;">
-    
+    <!-- <body class="sidebar-mini layout-boxed sidebar-collapse" style="height: auto;"> -->
+    <body>
         <!-- jQuery -->
-        <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+        <!-- <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script> -->
         <!-- Bootstrap -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
         <!--datatables-->
-        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+        <!-- <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script> -->
 
         <?php
         if ($act == "printing") {
