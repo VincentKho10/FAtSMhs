15ee0b983e786bb2080075690c8261a302fcaa98
VincentKho10 <vincentkho10@gmail.com>
2022-04-04 07:10:40 +0700

    finish

--- api/register/index.php
+++ api/register/index.php
@@ -9,7 +9,7 @@ try{
         $result_mahasiswa = $collection_mahasiswa->find(['nrp' => $nrp, 'password' => $pass]);
 		if($result_mahasiswa != null){
             $collection_mahasiswa_user = $client->fatsdb->mahasiswa_user;
-            $result_mahasiswa_user = $collection_mahasiswa->insertOne(['mahasiswa' => $result_mahasiswa, "mac_address" => $macaddr]);
+            $result_mahasiswa_user = $collection_mahasiswa_user->insertOne(['mahasiswa' => $result_mahasiswa, "mac_address" => $macaddr]);
             echo "{\"success\":success}";
         }else{
             echo "{\"success\":false,\"message\":\"mahasiswa result is null\"}";
