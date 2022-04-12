30a20e178686aea28f240bbb8941f11c1ab50e5f
VincentKho10 <vincentkho10@gmail.com>
2022-04-12 09:15:31 +0700

    test

--- api/register/index.php
+++ api/register/index.php
@@ -10,7 +10,7 @@ try{
 		if($result_mahasiswa != null){
             $collection_mahasiswa_user = $client->fatsdb->mahasiswa_user;
             $resinjson =  MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($result_mahasiswa));
-            $result_mahasiswa_user = $collection_mahasiswa_user->insertOne(["mahasiswa" => json_decode($resinjson), "mac_address" => $macaddr]);
+            $result_mahasiswa_user = $collection_mahasiswa_user->insertOne({"mahasiswa" => json_decode('.$resinjson.'), "mac_address" => '.$macaddr.'});
         }else{
             echo "{\"success\":false,\"message\":\"mahasiswa result is null\"}";
         }
